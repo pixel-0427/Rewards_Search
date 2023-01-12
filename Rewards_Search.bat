f9a34b10a062a15739395de983cc4d5c1cbf305a
Pixel <104253982+DotPixel0@users.noreply.github.com>
2023-01-12 18:09:18 +0900

    Add files via upload

---
+++ Rewards_Search.bat
@@ -0,0 +1,59 @@
+start https://www.bing.com/search?q=1
+timeout /nobreak 1
+start https://www.bing.com/search?q=2
+timeout /nobreak 1
+start https://www.bing.com/search?q=3
+timeout /nobreak 1
+start https://www.bing.com/search?q=4
+timeout /nobreak 1
+start https://www.bing.com/search?q=5
+timeout /nobreak 1
+start https://www.bing.com/search?q=6
+timeout /nobreak 1
+start https://www.bing.com/search?q=7
+timeout /nobreak 1
+start https://www.bing.com/search?q=8
+timeout /nobreak 1
+start https://www.bing.com/search?q=9
+timeout /nobreak 1
+start https://www.bing.com/search?q=10
+timeout /nobreak 1
+start https://www.bing.com/search?q=11
+timeout /nobreak 1
+start https://www.bing.com/search?q=12
+timeout /nobreak 1
+start https://www.bing.com/search?q=13
+timeout /nobreak 1
+start https://www.bing.com/search?q=14
+timeout /nobreak 1
+start https://www.bing.com/search?q=15
+timeout /nobreak 1
+start https://www.bing.com/search?q=16
+timeout /nobreak 1
+start https://www.bing.com/search?q=17
+timeout /nobreak 1
+start https://www.bing.com/search?q=18
+timeout /nobreak 1
+start https://www.bing.com/search?q=19
+timeout /nobreak 1
+start https://www.bing.com/search?q=20
+timeout /nobreak 1
+start https://www.bing.com/search?q=21
+timeout /nobreak 1
+start https://www.bing.com/search?q=22
+timeout /nobreak 1
+start https://www.bing.com/search?q=23
+timeout /nobreak 1
+start https://www.bing.com/search?q=24
+timeout /nobreak 1
+start https://www.bing.com/search?q=25
+timeout /nobreak 1
+start https://www.bing.com/search?q=26
+timeout /nobreak 1
+start https://www.bing.com/search?q=27
+timeout /nobreak 1
+start https://www.bing.com/search?q=28
+timeout /nobreak 1
+start https://www.bing.com/search?q=29
+timeout /nobreak 1
+start https://www.bing.com/search?q=30
